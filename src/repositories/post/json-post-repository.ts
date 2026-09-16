efb350894a59689a1e091690d18c5bcda8ae5ce2
Robson Junior <robson.fsj@gmail.com>
2026-09-16 15:58:48 -0300

    update undertanding repository pattern

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,16 +1,54 @@
 import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-repository";
+import { resolve } from "path";
+
+import { readFile } from "fs/promises";
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  "src",
+  "db",
+  "seed",
+  "posts.json",
+);
 
 export class JsonPostRepository implements PostRepository {
-  private async readFromDisk() {}
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
+    return posts;
+  }
 
   async findAll(): Promise<PostModel[]> {
-    throw new Error("Method not implemented.");
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+      throw new Error(`Post with id "${id}" not found`);
+    }
+
+    return post;
   }
 }
 
-export const postRepository = new JsonPostRepository();
+export const postRepository: PostRepository = new JsonPostRepository();
 
-console.log("Olá Mundo");
+//postRepository.findAll().then((jsonContent) => console.log(jsonContent));
+
+(async () => {
+  // const posts = await postRepository.findAll();
+  // posts.forEach((post) => {
+  //   console.log(post.id);
+  // });
+  // const post = await postRepository.findById(
+  //   "be3f14a1-0105-4e2e-bfc9-133a05e7bda6",
+  // );
+  // console.log(post);
+})();
