7bbe2e76a8298cd8a4952150419415ea03cab944
Robson Junior <robson.fsj@gmail.com>
2026-09-17 18:27:45 -0300

    update a component container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import { Container } from "@/components/Container";
 import { PostsList } from "@/components/PostList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div className="text-slate-900 bg-slate-100 min-h-screen dark:text-slate-100 dark:bg-slate-900">
+    <Container>
       <header>
-        <h1 className="text-6xl font-bold text-center p-8">Aqui é a header</h1>
+        <h1 className="text-6xl font-bold text-center py-8">Aqui é a header</h1>
       </header>
 
       <Suspense fallback={<SpinLoader />}>
@@ -16,6 +17,6 @@ export default async function HomePage() {
       <footer>
         <h1 className="text-6xl font-bold text-center p-8">footer</h1>
       </footer>
-    </div>
+    </Container>
   );
 }
