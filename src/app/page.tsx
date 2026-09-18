09f9fe2efc194f2c95c0896071f6049de2f70ffa
Robson Junior <robson.fsj@gmail.com>
2026-09-18 20:45:11 -0300

    update creating component postimagecover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { PostCoverImage } from "@/components/PostCoverImage";
 import { PostHeading } from "@/components/PostHeading";
 import { PostsList } from "@/components/PostList";
 import { SpinLoader } from "@/components/SpinLoader";
@@ -16,16 +17,18 @@ export default async function HomePage() {
       <section
         className={clsx("grid grid-cols-1 gap-8 mb-16", "sm:grid-cols-2 group")}
       >
-        <Link href={"#"} className="w-full h-full overflow-hidden rounded-xl">
-          <Image
-            className="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200 group-hover:rotate-3"
-            src={"/images/bryen_0.png"}
-            alt={"Title of post"}
-            width={1200}
-            height={720}
-            priority
-          ></Image>
-        </Link>
+        <PostCoverImage
+          linkProps={{
+            href: "/post/image",
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: "/images/bryen_9.png",
+            alt: "Titulo da imagem",
+            priority: true,
+          }}
+        />
         <div className="flex flex-col gap-4 sm:justify-center">
           <time
             className="text-slate-600 block text-sm/tight"
---
+++ src/components/PostCoverImage/index.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Image from "next/image";
+import clsx from "clsx";
+
+type PostCoverImageProps = {
+  imageProps: React.ComponentProps<typeof Image>;
+  linkProps: React.ComponentProps<typeof Link>;
+};
+
+export function PostCoverImage({ imageProps, linkProps }: PostCoverImageProps) {
+  return (
+    <Link
+      {...linkProps}
+      className={clsx(
+        "w-full",
+        "h-full",
+        "overflow-hidden",
+        "rounded-xl",
+        linkProps.className,
+      )}
+    >
+      <Image
+        {...imageProps}
+        className={clsx(
+          "w-full",
+          "h-full",
+          "object-cover",
+          "object-center",
+          "group-hover:scale-110",
+          "transition",
+          "duration-200",
+          "group-hover:rotate-3",
+          imageProps.className,
+        )}
+        alt={imageProps.alt}
+      ></Image>
+    </Link>
+  );
+}
